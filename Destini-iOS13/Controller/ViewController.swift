1dedf0b0047035831dfcc92a980cbcb502cbd8a8
Thien.nguyen <thien.nguyen@g-days.net>
2022-03-09 17:25:50 +0700

    OK

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Initialize View
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    // End view
+    
+    // Initialize var
+    var storyBrain = StoryBrain()
+    // End var
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // First time to load the app
+        storyLabel.text = storyBrain.seeFirstStory().0
+        choice1Button.setTitle(storyBrain.seeFirstStory().1, for: .normal)
+        choice2Button.setTitle(storyBrain.seeFirstStory().2, for: .normal)
+        // End first time
     }
 
-
+    // Event List
+    @IBAction func choiceMade(_ sender: UIButton) {
+        var nextStory = storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        storyLabel.text = nextStory.0
+        choice1Button.setTitle(nextStory.1, for: .normal)
+        choice2Button.setTitle(nextStory.2, for: .normal)    }
+    // End List
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,57 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    
+    // Initiazlie var
+    let longStory: [Story] = [
+        Story(
+            title: "You see a fork on the road.",
+            choice1: "Take a left.",
+            choice2: "Take a right."
+        ),
+        Story(
+            title: "You see a tiger.",
+            choice1: "Shout for help.",
+            choice2: "Play dead."
+        ),
+        Story(
+            title: "You find a treasure chest.",
+            choice1: "Open it.",
+            choice2: "Check for traps."
+        )]
+    // End var
+    
+    // Method List
+    // First story
+    func seeFirstStory() -> (String, String, String) {
+        return (
+            longStory[0].storyTitle,
+            longStory[0].choice1,
+            longStory[0].choice2
+        )
+    }
+    // End first
+    
+    // Nextstory
+    func nextStory(userChoice: String) -> (String, String, String) {
+        
+        // Check condition
+        if userChoice == longStory[0].choice1 {
+            return (
+                longStory[1].storyTitle,
+                longStory[1].choice1,
+                longStory[1].choice2
+            )
+        } else {
+            return (
+                longStory[2].storyTitle,
+                longStory[2].choice1,
+                longStory[2].choice2
+            )
+        }
+        // End condition
+    }
+    // End method
+    // End List
+}
